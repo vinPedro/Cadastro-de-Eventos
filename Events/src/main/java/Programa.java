6da144182b08f73656d76622e2d34eb873f3ff3e
Jefferson Brito <jeffersondanilo2517@gmail.com>
2025-02-13 04:48:15 -0300

    adicionando janela ao main

--- Events/src/main/java/Programa.java
+++ Events/src/main/java/Programa.java
@@ -1,17 +1,29 @@
 
-import com.Events.dao.Persitencia;
+import com.Events.dao.Persistencia;
 import com.Events.model.Adm;
-import com.Events.model.Evento;
-import com.Events.model.Gerenciador;
-import com.Events.model.LocalDateAdapter;
-import com.Events.model.Participante;
-import com.Events.model.Sexo;
+import com.Events.view.JanelaLogin;
+import com.Events.view.usuario.JanelaCadastroGerenciador;
 
 public class Programa {
 
 	public static void main(String[] args) throws Exception {
 
-		 LocalDateAdapter localDateAdapter = new LocalDateAdapter();
+		Adm adm = new Adm();
+		try{
+			adm = Persistencia.carregar();
+		}catch(Exception e){
+			adm = new Adm();
+			Persistencia.persistir(adm);
+		}
+
+		if(adm.getGerenciadores().size() > 0){
+			new JanelaLogin();
+		}else{
+			new JanelaCadastroGerenciador();
+		}
+
+		// Exemplos de uso
+		// LocalDateAdapter localDateAdapter = new LocalDateAdapter();
 
 		// Participante participante = new Participante("teste", "teste", 20,
 		// Sexo.Masculino);
@@ -30,15 +42,15 @@ public class Programa {
 
 		 //Gerenciador gerenciador = new Gerenciador("Teste2", "Teste2");
 		 //gerenciador.enviarCertificado(participante1, evento1);
-		 //Adm adm = new Adm();
-		Adm adm = Persitencia.carregar();
-		Gerenciador gerenciador = adm.recuperarAdm(0);
+		//  Adm adm = new Adm();
+		// Adm adm = Persitencia.carregar();
+		// Gerenciador gerenciador = adm.recuperarAdm(0);
 		// System.out.println(gerenciador.recuperarEvento(0));
 		// gerenciador.addAdm(grt1);
 
 		// evento.addparticipante(participante1);
-		Evento evento = gerenciador.recuperarEvento(0);
-		gerenciador.enviarCertificadoParticipantesEvento(evento);
+		// Evento evento = gerenciador.recuperarEvento(0);
+		// gerenciador.enviarCertificadoParticipantesEvento(evento);
 		// Evento evento1 = gerenciador.recuperarEvento(0);
 		// Participante participante = evento.recuperarParticipante(0);
 		// gerenciador.deleteparticipante(participante);
@@ -56,7 +68,7 @@ public class Programa {
 		// adm.addAdm(gerenciador);
 		// gerenciador.deleteparticipante(gerenciador.recuperarParticipante(0));
 		// gerenciador.deleteEvento(gerenciador.recuperarEvento(0));
-		Persitencia.persistir(adm);
+		// Persitencia.persistir(adm);
 
 		// gerenciador.gerarPDF(participante.getNomeString(), evento.getNomeString());
 
